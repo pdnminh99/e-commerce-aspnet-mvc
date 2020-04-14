50db70df0633289aba4982ddcfcaba8b28205b27
nhatminh <pdn.minh1999@gmail.com>
2020-04-14 16:07:02 +0700

    implement add to cart feat.

--- Models/Product.cs
+++ Models/Product.cs
@@ -10,25 +10,26 @@ namespace EcommerceApp2259.Models
     {
         public Guid ProductId { get; set; }
 
-        [Display(Name = "Tên sản phẩm")]
+        [Display(Name = "Product name")]
         public string Title { get; set; }
 
-        public List<ProductImage> ProductImages { get; set; }
+        [NotMapped]
+        public string Image { get; set; }
 
         public List<ProductDetail> Details { get; set; }
 
         public Guid? Owner { get; set; }
 
-        [Column("Price"), Display(Name = "Giá")]
+        [Column("Price"), Display(Name = "Price")]
         public int OriginalPrice { get; set; }
 
-        [DataType(DataType.DateTime), Display(Name = "Ngày tạo")]
+        [DataType(DataType.DateTime), Display(Name = "Created datetime")]
         public DateTime CreatedDate { get; }
 
-        [Display(Name = "Mục sản phẩm")]
+        [Display(Name = "Category")]
         public string Category { get; set; }
 
-        [Display(Name = "Thương hiệu")]
+        [Display(Name = "Brand")]
         public string Brand { get; set; }
 
         public int CompareTo(Product other) => OriginalPrice.CompareTo(other.OriginalPrice);
---
+++ wwwroot/js/app.js
@@ -0,0 +1,58 @@
+﻿// const shoppingCart = (function () {
+//     let cartString = localStorage.getItem("cart");
+//     if (cartString == null) {
+//         localStorage.setItem("cart", "{}");
+//         return {}
+//     }
+//     return JSON.parse(cartString)
+// })();
+//
+// const addProductToCart = ({productId, title, image, originalPrice, category, brand}, count) => {
+//     if (count == null) {
+//         count = 1;
+//     }
+//     let existingItem = shoppingCart[productId];
+//     if (existingItem == null) {
+//         shoppingCart[productId] = {productId, title, image, originalPrice, category, brand, count}
+//     } else {
+//         shoppingCart.count += count;
+//     }
+//     if (shoppingCart[productId].title !== title) {
+//         shoppingCart[productId].title = title;
+//     }
+//     if (shoppingCart[productId].image !== image) {
+//         shoppingCart[productId].image = image;
+//     }
+//     if (shoppingCart[productId].originalPrice !== originalPrice) {
+//         shoppingCart[productId].originalPrice = originalPrice;
+//     }
+//     if (shoppingCart[productId].category !== category) {
+//         shoppingCart[productId].category = category;
+//     }
+//     if (shoppingCart[productId].brand !== brand) {
+//         shoppingCart[productId].brand = brand;
+//     }
+//     localStorage.setItem("cart", JSON.parse(shoppingCart))
+// };
+//
+// const itemCountDisplayElement = document.getElementById("cart-item-count");
+// const totalPriceDisplayElement = document.getElementById("cart-total-price");
+//
+// const refreshShoppingCart = () => {
+//     let totalPrice = 0;
+//     let totalItemsCount = 0;
+//
+//     for (let itemId in Object.values(shoppingCart)) {
+//         totalPrice += shoppingCart[itemId].count * shoppingCart[itemId].originalPrice;
+//         totalItemsCount += shoppingCart[itemId].count;
+//     }
+//     itemCountDisplayElement.innerText = totalItemsCount + (totalItemsCount > 1 ? ' items' : 'item');
+//     totalPriceDisplayElement.innerText = `${totalPrice}$`
+// };
+//
+// function addItem(json) {
+//     addProductToCart(JSON.parse(json), 1);
+//     refreshShoppingCart();
+// }
+//
+// refreshShoppingCart();
