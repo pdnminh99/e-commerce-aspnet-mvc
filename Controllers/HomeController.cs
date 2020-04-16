43e6ea0ba9b8cba187d744df22fcbfd358c4eb35
nhatminh <pdn.minh1999@gmail.com>
2020-04-16 12:42:12 +0700

    add not found page and display product image in model in ProductDetail.cshtml

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -27,9 +27,9 @@ namespace EcommerceApp2259.Controllers
         }
 
 #nullable enable
-        public IActionResult Index(string? keyword)
+        public IActionResult Index()
         {
-            var products = keyword == null ? _productService.Get() : _productService.Get(keyword);
+            var products = _productService.Get();
             var brands = _brandService.Get();
             var categories = _categoryService.Get();
 
@@ -39,38 +39,62 @@ namespace EcommerceApp2259.Controllers
             return View(products);
         }
 
+        public IActionResult Products(String keyword)
+        {
+            var products = keyword == null ? 
+                _productService.Get() :
+                _productService.Get(keyword);
+            var brands = _brandService.Get();
+            var categories = _categoryService.Get();
+            ViewData["Manufacturers"] = brands;
+            ViewData["Categories"] = categories;
+            ViewData["OfferedProducts"] = products;
+            ViewData["HeadLine"] = $"{products.Count} products found.";
+            return View("Index", products);
+        }
+
         public IActionResult ProductDetail(Guid productId)
         {
             ViewData["SimilarProducts"] = _productService.Get();
             var product = _productService.Get(productId);
-            if (product == null) return NotFound();
             ViewData["Manufacturers"] = _brandService.Get();
             ViewData["Categories"] = _categoryService.Get();
             var products = _productService.Get();
             ViewData["OfferedProducts"] = products;
-            return View(product);
+            if (product != null) return View(product);
+            Response.StatusCode = 404;
+            return View("NotFound");
         }
 
         public IActionResult Category(int categoryId)
         {
             var category = _categoryService.Get(categoryId);
-            var allProducts = _productService.Get();
 
             ViewData["Manufacturers"] = _brandService.Get();
             ViewData["Categories"] = _categoryService.Get();
-            ViewData["OfferedProducts"] = allProducts;
+            ViewData["OfferedProducts"] = _productService.Get();
+            if (category == null)
+            {
+                Response.StatusCode = 404;
+                return View("NotFound");
+            }
             ViewData["HeadLine"] = $"Products by category {category.Name}.";
-            return View("Index", category?.Products);
+            return View("Index", category.Products);
         }
 
         public IActionResult Brand(int brandId)
         {
             var brand = _brandService.Get(brandId);
-            var allProducts = _productService.Get();
+            Console.WriteLine(brand);
 
             ViewData["Manufacturers"] = _brandService.Get();
             ViewData["Categories"] = _categoryService.Get();
-            ViewData["OfferedProducts"] = allProducts;
+            ViewData["OfferedProducts"] = _productService.Get();
+            if (brand == null)
+            {
+                Response.StatusCode = 404;
+                return View("NotFound");
+            }
             ViewData["HeadLine"] = $"Products by brand {brand.Name}.";
             return View("Index", brand?.Products);
         }
